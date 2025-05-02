e148edb36de0433cb90f48f276be46202d9748b7
TulikaSinha04 <131429123+TulikaSinha04@users.noreply.github.com>
2025-05-02 19:48:23 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
@@ -8,6 +11,8 @@ def init_driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     service = Service(ChromeDriverManager().install())
     return webdriver.Chrome(service=service, options=options)
 
@@ -15,22 +20,34 @@ def scrape_instagram_data(username):
     url = f"https://www.instagram.com/{username}/"
     driver = init_driver()
     driver.get(url)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+
     time.sleep(5)
 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
+    posts = followers = following = "Unknown"
     try:
-        data = soup.find_all('meta', property='og:description')[0]['content']
-        values = data.split(" - ")[0].split(', ')
-        posts = int(values[0].replace(',', '').split(' ')[0])
-        followers = values[1].split(' ')[0]
-        following = values[2].split(' ')[0]
+        spans = soup.find_all('span')
+        for span in spans:
+            text = span.get_text().replace(',', '')
+            if 'followers' in text.lower():
+                followers = text.split(' ')[0]
+            elif 'following' in text.lower():
+                following = text.split(' ')[0]
+            elif 'posts' in text.lower():
+                posts_text = text.split(' ')[0]
+                posts = ''.join(filter(str.isdigit, posts_text))
     except:
-        posts = followers = following = "Unknown"
+        pass
 
     post_links = []
     captions = []
     hashtags = []
+    likes_list = []
+    comments_list = []
+    images = []
+    engagement_rates = []
 
     links = soup.find_all('a')
     for link in links:
@@ -42,27 +59,96 @@ def scrape_instagram_data(username):
             if len(post_links) >= 10:
                 break
 
+    def convert_to_int(value_str):
+        try:
+            value_str = value_str.strip().lower().replace(',', '')
+            if 'k' in value_str:
+                return int(float(value_str.replace('k', '')) * 1000)
+            elif 'm' in value_str:
+                return int(float(value_str.replace('m', '')) * 1000000)
+            else:
+                return int(float(value_str))
+        except:
+            return 0
+
+    follower_count = convert_to_int(followers)
+
     for post_url in post_links:
         driver.get(post_url)
         time.sleep(3)
         post_soup = BeautifulSoup(driver.page_source, 'html.parser')
-        span = post_soup.find('meta', property='og:description')
-        if span:
-            caption = span['content']
-            captions.append(caption)
-            hashtags.append([tag for tag in caption.split() if tag.startswith('#')])
+
+        caption = ""
+        hash_tags = []
+        try:
+            desc_meta = post_soup.find('meta', property='og:description')
+            if desc_meta:
+                caption = desc_meta['content']
+                hash_tags = [tag for tag in caption.split() if tag.startswith('#')]
+        except:
+            pass
+
+        img_url = ""
+        try:
+            img_meta = post_soup.find('meta', property='og:image')
+            if img_meta:
+                img_url = img_meta['content']
+        except:
+            pass
+
+        captions.append(caption)
+        hashtags.append(hash_tags)
+        images.append(img_url)
+
+        likes = 0
+        comments = 0
+
+        try:
+            spans = post_soup.find_all('span')
+            for span in spans:
+                if 'like' in span.text.lower():
+                    text = span.text.strip().lower()
+                    if 'k' in text:
+                        likes = int(float(text.split()[0].replace('k', '')) * 1000)
+                    elif 'm' in text:
+                        likes = int(float(text.split()[0].replace('m', '')) * 1000000)
+                    else:
+                        likes = int(text.split()[0].replace(',', ''))
+                    break
+        except:
+            pass
+
+        try:
+            comment_blocks = post_soup.find_all('ul')
+            for block in comment_blocks:
+                li_tags = block.find_all('li')
+                comments = len(li_tags) - 1
+                break
+        except:
+            pass
+
+        likes_list.append(likes)
+        comments_list.append(comments)
+
+        if follower_count > 0:
+            engagement = ((likes + comments) / follower_count) * 100
+            engagement = round(min(engagement, 100.0), 2)
         else:
-            captions.append("")
-            hashtags.append([])
+            engagement = 0.0
+        engagement_rates.append(engagement)
 
     driver.quit()
 
     return {
         'username': username,
-        'posts': posts,
+        'posts': int(posts) if posts.isdigit() else 0,
         'followers': followers,
         'following': following,
         'post_links': post_links,
         'captions': captions,
-        'hashtags': hashtags
+        'hashtags': hashtags,
+        'likes': likes_list,
+        'comments': comments_list,
+        'images': images,
+        'engagement_rates': engagement_rates
     }
